34416491fdaa082ea137e9e4e6372dee3b75df67
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-10-18 19:51:18 +0200

    test github ci

--- build.bat
+++ build.bat
@@ -5,4 +5,4 @@ cd build_vc6
 echo %PATH%
 cmake.exe --version
 cmake.exe .. -G"Visual Studio 6"
-rem msdev decomp.dsw /MAKE "decomp_main - Win32 Release"
+msdev decomp.dsw /MAKE "decomp_main - Win32 Release"
